6ef6bfa73561f6c993e5af5ef7789154c2a9cf8b
Kalpana S <102243793+kalpana-webdesigner@users.noreply.github.com>
2024-11-10 16:30:52 +0530

    Update custom-taxonomy.php
    Creating Custom Taxonomy Without a Plugin

--- custom-taxonomy.php
+++ custom-taxonomy.php
@@ -1,1 +1,23 @@
 
+
+   // step : 1)  Open your functions.php file.
+   // step : 2) Add the following code to create a custom taxonomy:
+
+
+function create_custom_taxonomy() {
+    register_taxonomy(
+        'topics',  // Taxonomy key, must be unique
+        'posts',   // Object type: post, page, or your custom post type
+        array(
+            'labels' => array(
+                'name' => 'Topics',
+                'add_new_item' => 'Add New Topic',
+                'new_item_name' => "New Topic"
+            ),
+            'show_ui' => true,
+            'show_tagcloud' => false,
+            'hierarchical' => true  // True for categories-like, false for tags-like
+        )
+    );
+}
+add_action('init', 'create_custom_taxonomy');
